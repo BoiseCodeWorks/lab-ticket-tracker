943d2774a935c6ed6fdb48ea9c0a19c67cb96eaa
porterwilcox <wilcox.porter@gmail.com>
2019-03-29 14:53:51 -0600

    lab ticket tracker

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TicketTracker.Project;
+
+namespace TicketTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Ticket Tracker\nPress any key to continue");
+            Console.ReadLine();
+            App app = new App();
+            app.Run();
+        }
+    }
+}
